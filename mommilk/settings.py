2ec7eb276722a93f0cfcf576f9abcc75acec5318
joblackpoc <duopoc@gmail.com>
2024-12-21 17:15:36 +0700

    change static

--- mommilk/settings.py
+++ mommilk/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'    
 
 # Extra places for collectstatic to find static files.
