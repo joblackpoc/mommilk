a502fb818939c5c3baef7b5f12b2c8ac0f3ff112
joblackpoc <duopoc@gmail.com>
2024-12-21 11:11:53 +0700

    staticfiles

--- mommilk/urls.py
+++ mommilk/urls.py
@@ -33,4 +33,6 @@ urlpatterns = [
     path('', include('main.urls')),
     path('tinymce/', include('tinymce.urls')),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
-]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
