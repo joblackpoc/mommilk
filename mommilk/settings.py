659aad63b5c809e5765c48438051fcc75bf863c8
joblackpoc <duopoc@gmail.com>
2024-12-21 17:25:06 +0700

    collectstatic

--- mommilk/settings.py
+++ mommilk/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'    
-
+STATIC_ROOT = BASE_DIR / 'static/'
 # Extra places for collectstatic to find static files.
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'static'),
